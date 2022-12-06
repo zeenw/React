6760305a93db081b38206ba44d35a460a525fc1e
zeenw <111456142+zeenw@users.noreply.github.com>
2022-12-06 10:22:33 -0500

    add1

---
+++ fsd-medic/page/Home.js
@@ -0,0 +1,13 @@
+
+
+function Home() {
+    return (
+        <>
+            
+            <h1>Home Page</h1>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            
+        </>
+    );
+}
+export default Home;
